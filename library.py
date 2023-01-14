2a9a0509aa424ade5ec05424c260b6abb5beae1c
Chandra <68412503+Sam1431@users.noreply.github.com>
2023-01-14 18:06:35 +0530

    Create library.py

---
+++ library.py
@@ -0,0 +1,44 @@
+def register():
+        Sno = []
+        Name = []
+        Id = []
+        Bookname = []
+        Duration = []
+        Amt = []
+
+        entries = int(input("Total Entries : "))
+        for x in range(entries):
+            sno = int(input("Enter serial number : "))
+            Sno.append(sno)
+
+            name = input("Enter member name  : ")
+            Name.append(name)
+
+            mid = input("Enter member ID : ")
+            Id.append(mid)
+
+            bname = int(input("Enter name of the Book : "))
+            Bname.append(bname)
+
+            dur = input("Enter duration for Borrowing : ")
+            Duration.append(dur)
+
+            amt = input("Enter amount : ")
+            Amt.append(amt)
+        
+        dict = {
+            "Sno": Sno,
+            "Name": Name,
+            "ID": Id,
+            "Book name": Bname,
+            "Duration": Duration,
+            "Amount": Tclass,
+        }
+        libdf = pd.DataFrame(dict)
+        print(libdf, "\n")
+        save = input("Would You like to save : ")
+        if save == "y" or "Y":
+            fil_name = input("Enter File Name : ")
+            libdf.to_csv("csv/railway/" + fil_name + ".csv")
+        else:
+            print("Not Saving")
